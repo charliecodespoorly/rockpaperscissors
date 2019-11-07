30e42933f2bf04b2f3a682c4797c3e58a7391ad4
Charles Kvoriak <charliemckennakvoriak@gmail.com>
2019-11-06 18:28:33 -0800

    Implemented rock, paper, scissors, spock, lizard

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@
 
 $(document).ready(function (){
     $("#shoot").click(function() {
-      
       var userPlayString = $("#input").val();
       userPlayString = userPlayString.toLowerCase();
       var userPlay;
+      $("#userChoice").text(userPlayString);
 
       switch(userPlayString){
         case "rock":
@@ -26,8 +26,65 @@ $(document).ready(function (){
         case "scissors":
           userPlay = 2;
           break;
+        case "spock":
+          userPlay = 3;
+          break;          
+        case "lizard":
+          userPlay = 4;
+          break;
+
+
+        default:
+          alert("this is not an option, please enter rock, paper, or scissors");
       }
 
-      console.log(userPlay);
+      var computerPlay = Math.floor(Math.random()*5);
+      var computerPlayString;
+      switch(computerPlay){
+        case 0:
+          computerPlayString="rock";
+          break;
+        case 1:
+          computerPlayString="paper";
+          break;
+        case 2:
+          computerPlayString="scissors";
+          break;
+        case 3:
+          computerPlayString="spock";
+          break;
+        case 4:
+          computerPlayString="lizard";
+          break;
+      }
+      $("#computerChoice").text(computerPlayString);
+
+      var humanWins = 0; //0 for lose, 1 for tie, 2 for win
+      if((computerPlay + 1)%5 === userPlay){
+        humanWins = 2;
+        $("#result").text("You win!");
+        console.log("You win!");
+      }
+      else if((userPlay+1)%5 === computerPlay){
+        humanWins = 0;
+        $("#result").text("You lose!");
+        console.log("You lose!");
+      }
+
+      if((computerPlay + 2)%5 === userPlay){
+        humanWins = 0;
+        $("#result").text("You lose!");
+        console.log("You win!");
+      }
+      else if((userPlay+2)%5 === computerPlay){
+        humanWins = 2;
+        $("#result").text("You win!");
+        console.log("You lose!");
+      }
+      else if(userPlay === computerPlay){
+        humanWins = 1;
+        $("#result").text("Tie!");
+        console.log("Tie!");
+      }
     });
 });
